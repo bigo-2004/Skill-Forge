7d34f9aab31988ca001c3aa5f1205d9e5cae4c5e
Seif Diaa <eidsaif308@gmail.com>
2025-11-16 15:00:01 +0200

    CreateCourse added

---
+++ Skill-Forge/src/SkillForge/CreateCourseForm.java
@@ -0,0 +1,9 @@
+package SkillForge;
+
+import javax.swing.*;
+
+public class CreateCourseForm {
+    private JButton saveButton;
+    private JTextField textField1;
+    private JTextField textField2;
+}

7d28b9ab611eb14c28f90b6af28edc83f6beac66
Youssef Ramadan <eng._.youssef@icloud.com>
2025-11-16 16:02:28 +0200

    Edited forms

--- Skill-Forge/src/SkillForge/InstructorForm.java
+++ Skill-Forge/src/SkillForge/InstructorForm.java
@@ -1,6 +1,8 @@
 package SkillForge;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InstructorForm extends JFrame {
     private JButton createNewCourseButton;
@@ -17,6 +19,13 @@ public class InstructorForm extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new LoginForm();
+            }
+        });
     }
 
 }

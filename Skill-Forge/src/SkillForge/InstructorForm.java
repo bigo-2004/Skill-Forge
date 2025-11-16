b11af92802bbf71adcdf71ee0abe171262905670
Seif Diaa <eidsaif308@gmail.com>
2025-11-16 14:52:47 +0200

    InstructorForm added

---
+++ Skill-Forge/src/SkillForge/InstructorForm.java
@@ -0,0 +1,10 @@
+package SkillForge;
+
+import javax.swing.*;
+
+public class InstructorForm {
+    private JButton createNewCourseButton;
+    private JButton listOfCoursesButton;
+    private JButton manageCourseButton;
+    private JButton logoutButton;
+}

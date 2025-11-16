2b2bc249aa391c5779fa53426927cd9e6b6e9f10
ahmed_rabie_eg <eizomizo12@gmail.com>
2025-11-16 19:31:32 +0200

    Added Course and Lesson classes

---
+++ Skill-Forge/src/SkillForge/Lesson.java
@@ -0,0 +1,15 @@
+﻿package SkillForge;
+
+public class Lesson
+{
+    String lessonID;
+    String lessonTitle;
+    String lessonContent;
+    String courseID;
+
+
+    public void markAsWatched(User student)
+    {
+        //TODO
+    }
+}

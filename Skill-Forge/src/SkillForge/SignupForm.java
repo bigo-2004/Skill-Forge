545af179f44f73f6a5129be441554a8eff130e0b
Abdelrahman Ibrahim Mohamed Ragab <es.abdelrahman.kotb2023@alexu.edu.eg>
2025-11-16 18:30:39 +0200

    Added rest of validations and fixed id to a certain number and increments every time

--- Skill-Forge/src/SkillForge/SignupForm.java
+++ Skill-Forge/src/SkillForge/SignupForm.java
@@ -1,10 +1,13 @@
 package SkillForge;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SignupForm extends JFrame{
+public class SignupForm extends JFrame {
     private JButton createAccountButton;
     private JTextField textField1;
     private JTextField textField2;
@@ -28,6 +31,25 @@ public class SignupForm extends JFrame{
         setLocationRelativeTo(null);
         setVisible(true);
 
+        UserJsonDatabase db = new UserJsonDatabase("users.json");
+        String id;
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = db.readJsonArrayFromFile();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            jsonObject = jsonArray.getJSONObject(i);
+        }
+        if(jsonArray.length()>0) {
+            String lastId = jsonObject.getString("id");
+            int lId = Integer.parseInt(lastId);
+            lId++;
+            id = String.valueOf(lId);
+        }
+        else {
+            id = String.valueOf(1);
+        }
+        textField4.setText(id);
+        textField4.setEditable(false);
+
 
         returnToLoginPageButton.addActionListener(new ActionListener() {
             @Override
@@ -39,36 +61,55 @@ public class SignupForm extends JFrame{
         createAccountButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+
                 String username = textField1.getText();
                 String email = textField2.getText();
                 String pd = textField3.getText();
-                String id =textField4.getText();
-                String role=null;
-                if(studentRadioButton.isSelected()){
-                    role="Student";
-                }
-                else if(instructorRadioButton.isSelected()){
-                    role="Instructor";
+
+                String id = textField4.getText();
+
+                String role = null;
+                if (studentRadioButton.isSelected()) {
+                    role = "Student";
+                } else if (instructorRadioButton.isSelected()) {
+                    role = "Instructor";
                 }
 
-                if (username.isEmpty() || email.isEmpty() || pd.isEmpty()  || (!studentRadioButton.isSelected() && !instructorRadioButton.isSelected()))
-                {
+                if (username.isEmpty() || email.isEmpty() || pd.isEmpty() || (!studentRadioButton.isSelected() && !instructorRadioButton.isSelected())) {
                     JOptionPane.showMessageDialog(null, "Please fill all fields and select a role.");
                     return;
                 }
 
-                if(SigningOperations.signup(id ,username,email,pd,role)){
-                    JOptionPane.showMessageDialog(null, "User added successfully");
+                if (!Validations.isValidName(username) || username.length() < 3) {
+                    JOptionPane.showMessageDialog(null, "Invalid username!");
+                    return;
                 }
-                else{
-                    JOptionPane.showMessageDialog(null, "User already exist");
+
+
+
+
+                if (!Validations.isValidEmail(email)) {
+                    javax.swing.JOptionPane.showMessageDialog(null, "Invalid email!");
+                    return;
+                }
+
+                if (!Validations.isValidPassword(pd)) {
+                    JOptionPane.showMessageDialog(null, "Password must be at least 8 characters and contain at least one capital letter.");
+                    return;
+                }
+
+                if (SigningOperations.signup(id, username, email, pd, role)) {
+                    JOptionPane.showMessageDialog(null, "User added successfully");
+                } else {
+                    JOptionPane.showMessageDialog(null, "A user with this email already exist");
+                    return;
                 }
                 dispose();
                 new LoginForm();
             }
         });
     }
-
 
 
 }
--- Skill-Forge/src/SkillForge/Validations.java
+++ Skill-Forge/src/SkillForge/Validations.java
@@ -15,4 +15,25 @@ public class Validations {
         return matcher.matches();
 
     }
+
+    public static boolean isValidPassword(String password) {
+        String regex = "^(?=.*[A-Z]).{8,}$";
+        if(password.matches(regex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isValidName(String name) {
+        name = name.trim();
+        if (name.isEmpty()) return false;
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (!Character.isLetter(c) && c != ' '  && c != '\'') {
+                return false;
+            }
+        }
+        return true;
+    }
 }

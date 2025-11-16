2e1d3147a4a3b0aa5f3a7be5fda56a4ef36564fc
Seif Diaa <eidsaif308@gmail.com>
2025-11-16 14:59:25 +0200

    AddLessonForm added

---
+++ Skill-Forge/src/SkillForge/AddLessonForm.java
@@ -0,0 +1,9 @@
+package SkillForge;
+
+import javax.swing.*;
+
+public class AddLessonForm {
+    private JButton saveButton;
+    private JTextField textField1;
+    private JTextField textField2;
+}

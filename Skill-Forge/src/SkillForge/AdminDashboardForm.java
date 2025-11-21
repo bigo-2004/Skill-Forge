f6c9cdd9a0f66cc50aa929f45a8bfa2ded223040
Seif Diaa <eidsaif308@gmail.com>
2025-11-21 13:55:35 +0200

    AdminDashboardForm added

---
+++ Skill-Forge/src/SkillForge/AdminDashboardForm.java
@@ -0,0 +1,10 @@
+package SkillForge;
+
+import javax.swing.*;
+
+public class AdminDashboardForm {
+    private JButton viewPendingCoursesButton;
+    private JPanel panel1;
+    private JButton viewAllCoursesButton;
+    private JButton logoutButton;
+}

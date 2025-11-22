43ec020d7c43f2c878a8b998ec4e43e641473dd7
Seif Diaa <eidsaif308@gmail.com>
2025-11-22 23:05:32 +0200

    edit admin form

---
+++ Skill-Forge/src/SkillForge/AdminForm.java
@@ -0,0 +1,48 @@
+package SkillForge;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminForm extends JFrame {
+    private JButton viewPendingCoursesButton;
+    private JPanel panel1;
+    private JButton viewAllCoursesButton;
+    private JButton logoutButton;
+
+    private User admin;
+
+    public AdminForm(User admin) {
+        this.admin = admin;
+
+        setTitle("Admin Dashboard");
+        setContentPane(panel1);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        viewPendingCoursesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new ViewPendingCoursesForm();
+            }
+        });
+
+        viewAllCoursesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(AdminForm.this, "Coming soon...");
+            }
+        });
+
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new LoginForm();
+            }
+        });
+    }
+}

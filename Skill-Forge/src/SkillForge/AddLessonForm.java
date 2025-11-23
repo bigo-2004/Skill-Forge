c48e93a7edc572b07c5514da049882bd1f3c7b73
Abdelrahman Ibrahim Mohamed Ragab <es.abdelrahman.kotb2023@alexu.edu.eg>
2025-11-23 16:03:27 +0200

    Made lesson id not editable and generated

--- Skill-Forge/src/SkillForge/AddLessonForm.java
+++ Skill-Forge/src/SkillForge/AddLessonForm.java
@@ -14,6 +14,7 @@ public class AddLessonForm extends JFrame {
     private JPanel mainPanel;
     private JTextField textField3;
     private Course course;
+    private String lessonID = "L1";
 
     public AddLessonForm(Course course){
         this.course = course;
@@ -23,6 +24,28 @@ public class AddLessonForm extends JFrame {
         setSize(400, 250);
         setLocationRelativeTo(null);
         setVisible(true);
+        CourseJsonDatabase db = new CourseJsonDatabase("courses.json");
+        JSONArray  jsonArray = db.readJsonArrayFromFile();
+        JSONObject courseObject = course.toJson();
+        if (!courseObject.has("lessons")) ;
+        else {
+            JSONArray lessonsArray = courseObject.getJSONArray("lessons");
+            JSONObject lessonObject = new JSONObject();
+            int flag=0;
+            for (int j = 0; j < lessonsArray.length(); j++) {
+                lessonObject = (JSONObject) lessonsArray.get(j);
+                flag = 1;
+            }
+            if(flag==1){
+                String lessId = lessonObject.getString("lessonId");
+                int num = Integer.parseInt(lessId.substring(1));
+                this.lessonID = "L" + String.valueOf(num+1);
+            }
+
+
+        }
+        textField3.setEditable(false);
+        textField3.setText(this.lessonID);
 
         saveButton.addActionListener(new ActionListener() {
             @Override
@@ -46,20 +69,20 @@ public class AddLessonForm extends JFrame {
         //for (int i = 0; i < jsonArray.length(); i++) {
 
 
-
-            JSONObject courseObject = course.toJson();
-            if (!courseObject.has("lessons")) ;
-            else {
-            JSONArray lessonsArray = courseObject.getJSONArray("lessons");
-            for(int j = 0; j < lessonsArray.length(); j++) {
-                JSONObject lessonObject = (JSONObject) lessonsArray.get(j);
-                String lessonID = lessonObject.getString("lessonId");
-                if (lessonID.equals(textField3.getText())) {
-                    JOptionPane.showMessageDialog(this, "Lesson with this ID already exists.");
-                    return;
-                }
-            }
-            }
+//
+//            JSONObject courseObject = course.toJson();
+//            if (!courseObject.has("lessons")) ;
+//            else {
+//            JSONArray lessonsArray = courseObject.getJSONArray("lessons");
+//            for(int j = 0; j < lessonsArray.length(); j++) {
+//                JSONObject lessonObject = (JSONObject) lessonsArray.get(j);
+//                String lessonID = lessonObject.getString("lessonId");
+//                if (lessonID.equals(textField3.getText())) {
+//                    JOptionPane.showMessageDialog(this, "Lesson with this ID already exists.");
+//                    return;
+//                }
+//            }
+//            }
 
 
 
